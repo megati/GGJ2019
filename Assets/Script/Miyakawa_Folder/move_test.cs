77d888ac7eed9e571a9bbde220e3223ae4ea1c4e
Miyakawa <m0116282d3@edu.teu.ac.jp>
2019-01-26 14:51:42 +0900

    猫の風のパーティクル完成しました。星にするかどうかは実際に入れてみてから要検討。

---
+++ Assets/Script/Miyakawa_Folder/move_test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        transform.Translate(Vector3.forward/2);
+    }
+}
