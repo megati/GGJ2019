e65d7b00b1480818ade0c5102a57e4b3a2c6fac8
ohmori <houki1221@yahoo.co.jp>
2019-01-27 01:33:12 +0900

    ランダムマップシステム

--- Assets/Source/Game/UI/BreakBuildingText.cs
+++ Assets/Source/Game/UI/BreakBuildingText.cs
@@ -12,10 +12,7 @@ public class BreakBuildingText : MonoBehaviour
     private Text text;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
