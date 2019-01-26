fa015da48951652ae14c4d08f1872dad5971e1ab
shingoaoki <sp4blue@gmail.com>
2019-01-27 03:22:55 +0900

    ビルに当たったら、ｈｐがへる

---
+++ Assets/Source/Game/BillHitToCat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillHitToCat : MonoBehaviour
+{
+    private List<GameObject> hitObjectList = new List<GameObject>();
+
+    public bool IsHit
+    {
+        get
+        {
+            return hitObjectList.Count > 0;
+        }
+    }
+
+    public GameObject PopupHitObject()
+    {
+        if (IsHit == true)
+        {
+            GameObject gameObject = hitObjectList[0];
+            hitObjectList.Remove(gameObject);
+            return gameObject;
+        }
+        return null;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(""+collision.gameObject.GetComponent<BulletBase>() != null);
+        if ((collision.gameObject.GetComponent<BulletBase>() != null) || (collision.gameObject.GetComponent<HitJoint>() != null) && IsHit == false) {
+            Debug.Log("HIT!!!!!!!");
+            hitObjectList.Add(collision.gameObject);
+        }
+    }
+
+}
