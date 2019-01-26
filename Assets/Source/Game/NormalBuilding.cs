65870c3f31dd106b224e3d5f4bd83e1ff3d2a1b6
ohmori <houki1221@yahoo.co.jp>
2019-01-26 09:35:56 +0900

    ゲーム画面更新

---
+++ Assets/Source/Game/NormalBuilding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalBuilding : BuildingBase
+{
+    //コンストラクタ
+    NormalBuilding(int life, int bulletNum)
+    {
+        this.life = life;
+        this.recoveryBulletNum = bulletNum;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
