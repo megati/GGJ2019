d17f108fbe206ef1c86b7fb68b45ef76cffb959f
shingoaoki <sp4blue@gmail.com>
2019-01-26 20:18:57 +0900

    あたったらくっつく

---
+++ Assets/Source/Game/CatBullet.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBullet : BulletBase
+{
+    private Vector3 velocitySpeed = Vector3.zero;
+    [SerializeField]
+    private float firstSpeed = 0;
+    [SerializeField]
+    private Rigidbody rigidBody = null;
+    [SerializeField]
+    private List<HitJoint> hitJoints = new List<HitJoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //velocitySpeed = this.transform.forward * firstSpeed;
+        velocitySpeed = Vector3.forward * firstSpeed;
+        rigidBody.AddForce( velocitySpeed, ForceMode.Impulse );
+        var hits = GetComponentsInChildren<HitJoint>();
+        for( int i = 0; i < hits.Length; ++i )
+        {
+            hitJoints.Add(hits[i]);
+        }
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        Vector3 direction = rigidBody.velocity.normalized;
+        this.transform.rotation = Quaternion.Euler(direction);
+        bool isHit = false;
+        GameObject hitObject = null;
+        foreach ( var hitJoint in hitJoints )
+        {
+            if (hitJoint.isHit == true)
+            {
+                isHit = true;
+                hitObject = hitJoint.HitObject;
+            }
+        }
+
+        if (isHit == true)
+        {
+            SetJoint(hitObject);
+            Destroy(this);
+        }
+
+
+    }
+    
+    void SetJoint(GameObject hitObjent)
+    {
+        Debug.Log("Set Joint");
+        foreach (var hitJoint in hitJoints)
+        {
+            if (hitObjent.GetComponent<Rigidbody>() != null)
+            {
+                var hinge = hitObjent.AddComponent<HingeJoint>();
+                hinge.connectedBody = GetComponent<Rigidbody>();
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+}
