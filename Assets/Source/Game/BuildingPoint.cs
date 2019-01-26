305263cad0ad398bc254121196ff992eb135b1a2
ohmori <houki1221@yahoo.co.jp>
2019-01-26 19:25:27 +0900

    ビルの作成

---
+++ Assets/Source/Game/BuildingPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ビル生成場所管理
+/// </summary>
+public class BuildingPoint : MonoBehaviour
+{
+    [SerializeField]
+    //生成場所番号
+    private int number = 0;
+
+    /// <summary>
+    /// 生成場所番号を返す
+    /// </summary>
+    public int GetNumber() { return number; }
+}
