2b9c063e2b1b8d99281bd63112753a5a50d487a1
Miyakawa <m0116282d3@edu.teu.ac.jp>
2019-01-27 15:58:24 +0900

    ゲーム説明シーンの調整

---
+++ Assets/Script/Miyakawa_Folder/RuleToGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuleToGame : MonoBehaviour
+{
+    public bool ispush;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void GoToGame()
+    {
+        if (ispush == false)
+        {
+            ispush = true;
+            SceneManager.LoadScene("GameScene");
+            return;
+        }
+    }
+}
