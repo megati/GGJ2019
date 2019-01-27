218657567c35abd641b4463d336e9baa152c79f6
Miyakawa <m0116282d3@edu.teu.ac.jp>
2019-01-27 09:28:23 +0900

    チームロゴシーンからタイトルへの流れができました。

---
+++ Assets/Script/Miyakawa_Folder/LogoManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("LogoScene", 3.0f);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void LogoScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
