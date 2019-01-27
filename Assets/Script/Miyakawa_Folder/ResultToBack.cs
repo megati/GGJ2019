de2e677d461a3ad4d266b1f7f355a5a746e1a4de
Miyakawa <m0116282d3@edu.teu.ac.jp>
2019-01-27 09:47:57 +0900

    リザルト～スタッフロール～タイトルまでのシーン切り替えできました

---
+++ Assets/Script/Miyakawa_Folder/ResultToBack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultToBack : MonoBehaviour
+{
+    public bool ispush;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void BackToTitle()
+    {
+        if (ispush == false)
+        {
+            ispush = true;
+            SceneManager.LoadScene("StaffRollScene");
+            return;
+        }
+    }
+}
